3b62c44f978ef7b4eefe6a76e0bd2ee4a542471d
moreshwarnabar <mrnabar@gmail.com>
2025-03-13 12:58:41 -0700

    vectorized overview and saved df

--- processing/preprocess.py
+++ processing/preprocess.py
@@ -1,4 +1,6 @@
+import os
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def process_features(movies: list, genre_df: pd.DataFrame) -> None:
     if not movies:
@@ -22,6 +24,26 @@ def process_features(movies: list, genre_df: pd.DataFrame) -> None:
 
     df = pd.DataFrame(extracted_data)
     
-    for genre in genre_df["name"].tolist():
+    df = encodeGenres(df, genre_df["name"].to_list())
+    df = vectorizeOverview(df)
+
+    if os.path.exists("data/preprocessed_data.csv"):
+        preprocessed_df = pd.read_csv("data/preprocessed_data.csv")
+        preprocessed_df = pd.concat([preprocessed_df, df], axis=0)
+        preprocessed_df.to_csv("data/preprocessed_data.csv", index=False)
+    else:
+        df.to_csv("data/preprocessed_data.csv", index=False)
+
+def encodeGenres(df: pd.DataFrame, genres: list) -> pd.DataFrame:
+    for genre in genres:
         df[genre] = df["genres"].apply(lambda x: 1 if genre in x else 0)
-    df = df.drop("genres", axis=1)
+    return df.drop("genres", axis=1)
+
+def vectorizeOverview(df: pd.DataFrame) -> pd.DataFrame:
+    vectorizer = TfidfVectorizer()
+    overview_vectors = vectorizer.fit_transform(df["overview"])
+
+    overview_df = pd.DataFrame(overview_vectors.toarray(), columns=vectorizer.get_feature_names_out())
+    df = df.drop("overview", axis=1)
+
+    return pd.concat([df, overview_df], axis=1)
