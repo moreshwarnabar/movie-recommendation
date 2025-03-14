827427da74e23b473b76aec37d5fb3d55935a8fd
moreshwarnabar <mrnabar@gmail.com>
2025-03-13 21:40:26 -0700

    collecting 250 pages of data

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import pandas as pd
 from dotenv import load_dotenv
 from ingestion.fetch_movies import get_genres, get_movie_data
-from processing.preprocess import process_features, store_features
+from processing.preprocess import process_features
+from processing.feature_store import store_features
 
 load_dotenv()
     
 def main():
     genres: pd.DataFrame = get_genres()
 
-    page_num: int
-    movies: list
-    page_num, movies = get_movie_data()
-
-    if movies:
+    for page in range(1, 251):
+        movies: list = get_movie_data(page)
         df: pd.DataFrame = process_features(movies, genres)
         store_features(df)
 
