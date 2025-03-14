9267fdc23a2b9f4abed3ac28c33167582eb5e5ac
moreshwarnabar <mrnabar@gmail.com>
2025-03-13 21:32:48 -0700

    updated storing features to check for previous data

--- processing/feature_store.py
+++ processing/feature_store.py
@@ -1,5 +1,10 @@
+import os
 import pandas as pd
 
 def store_features(df: pd.DataFrame) -> None:
-    df.to_parquet('data/features.parquet', 
-                  engine='pyarrow', index=False)
+    if os.path.exists("data/features.parquet"):
+        all_data = pd.read_parquet("data/features.parquet", engine="pyarrow")
+        all_data = pd.concat([all_data, df], ignore_index=True)
+        all_data.to_parquet("data/features.parquet", engine="pyarrow", index=False)
+    else:
+        df.to_parquet("data/features.parquet", engine="pyarrow", index=False)
